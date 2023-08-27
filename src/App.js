75387800629699ed86599650b6b6401562e5e89e
kevin <kevinngugi197@gmail.com>
2023-08-28 00:39:46 +0300

    Added Sign-up and Sign-in

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import Home from "./pages/Home";
 import Nopage from "./pages/No-page";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import SignIn from "./pages/SIgn-in";
+import SignUp from "./pages/SIgn-up";
 
 function App() {
   return (
     <>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<Nopage />} />
-    </Routes>
-    <Footer />
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/sign-up" element={<SignUp />} />
+        <Route exact path="/sign-in" element={<SignIn />} />
+        <Route exact path="*" element={<Nopage />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
